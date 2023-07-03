0dd05b8b9c36d2e8ddcb93fc4f85fe819e82eb55
emifa <emifa@LAPTOP-1TM449QB>
2023-07-03 15:49:32 +0200

    Aggiungere i file di progetto.

---
+++ Esercizio-1-LINQ- .NET/GenerateRandomNumb.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Esercizio_1_LINQ__.NET
+{
+    internal class GenerateRandomNumb
+    {
+        public List<int> GenerateNumb(int n)
+        {
+            Random random = new Random();
+            List<int> list = new List<int>();
+            for (int i = 0; i < n; i++)
+            {
+                int randomNum = random.Next();
+                list.Add(randomNum);
+                Console.WriteLine(list[i]);
+            }
+            return list;
+        }
+
+        public void SelectUnevenNumb(List<int> list)
+        {
+            var unevenNumebr = from number in list
+                               where number % 2 != 0
+                               select number;
+            foreach (var number in unevenNumebr)
+            Console.WriteLine(number);
+        }
+    }
+}
---
+++ Esercizio-1-LINQ- .NET/Program.cs
@@ -0,0 +1,18 @@
+﻿using Esercizio_1_LINQ__.NET;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        GenerateRandomNumb generateRandomNumb = new GenerateRandomNumb();
+        List<int> randomNum = generateRandomNumb.GenerateNumb(10);
+        Console.WriteLine();
+        Console.WriteLine("Numeri dispari: \n");
+
+        generateRandomNumb.SelectUnevenNumb(randomNum);
+        
+            
+    
+            
+    }
+}
